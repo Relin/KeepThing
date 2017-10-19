0284c8362d59e00a79516a7c6a679ff601e7b10c
Sprumont Raphaël <raphael.sprumont@gmail.com>
2017-10-19 16:58:25 +0200

    init

---
+++ src/main/java/be/relin/dev/KeepThing/KeepThingApplication.java
@@ -0,0 +1,12 @@
+package be.relin.dev.KeepThing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepThingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepThingApplication.class, args);
+	}
+}
